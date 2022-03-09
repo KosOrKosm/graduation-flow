2f217fe7adbdddfa522b0febd86c6b7e7ef03628
anthony10154 <soriano_anthony473@csu.fullerton.edu>
2022-03-09 10:02:43 -0800

    experimental version

--- js/main-canvas.js
+++ js/main-canvas.js
@@ -1,49 +1,91 @@
-
-
 class FlowNode {
 
-    constructor() {
+    // Public node fields
+    x = 0
+    y = 0
 
-        this.x = 0
-        this.y = 0
-        this.beingDragged = false
+    className = "Introduction to Mathmatics"
+    classCode = "MATH101"
 
-    }
+    // Private + static node fields
+    static #sizeX = 100
+    static #sizeY = 70
+    static #tabSizeY = 20
+    static #textPadding = 3
 
-    setX(newX) {
-        this.x = newX
-    }
+    constructor(x, y, tabColor) {
 
-    setY(newY) {
-        this.y = newY
+        this.x = x
+        this.y = y
+        this.tabColor = tabColor
+
     }
 
     isInVolume(_x, _y) {
-        return this.x <= _x && this.y <= _y && this.x + 100 >= _x && this.y + 20 >= _y
+        return  this.x <= _x && 
+                this.y <= _y && 
+                this.x + FlowNode.#sizeX >= _x && 
+                this.y + FlowNode.#sizeY >= _y
+    }
+
+    isInTabVolume(_x, _y) {
+        return  this.x <= _x && 
+                this.y <= _y && 
+                this.x + FlowNode.#sizeX >= _x && 
+                this.y + FlowNode.#tabSizeY >= _y
     }
 
     draw() {
+        stroke('rgb(0,0,0)')
+        strokeWeight(2)
         fill('white')
-        rect(this.x, this.y, 100, 70, 5)
-        fill('maroon')
-        rect(this.x, this.y, 100, 20, 5)
+        rect(this.x, this.y, FlowNode.#sizeX, FlowNode.#sizeY, 5)
+        fill(this.tabColor)
+        rect(this.x, this.y, FlowNode.#sizeX, FlowNode.#tabSizeY, 5)
+
+        strokeWeight(0)
+        textSize(FlowNode.#tabSizeY - FlowNode.#textPadding * 2)
+        textAlign(CENTER, TOP)
+        fill('black')
+        text(
+            this.classCode, 
+            this.x + FlowNode.#textPadding,
+            this.y + FlowNode.#textPadding,
+            FlowNode.#sizeX - FlowNode.#textPadding * 2,
+            FlowNode.#sizeY - FlowNode.#textPadding * 2,
+        )
+        textAlign(CENTER, CENTER)
+        text(
+            this.className, 
+            this.x + FlowNode.#textPadding,
+            this.y + FlowNode.#tabSizeY + FlowNode.#textPadding,
+            FlowNode.#sizeX - FlowNode.#textPadding * 2,
+            FlowNode.#sizeY - FlowNode.#tabSizeY - FlowNode.#textPadding,
+        )
     }
 }
 
 var canvas = null
 let nodes = []
+let dragging = false
+let currently_dragged = null
+let drag_offx = 0, drag_offy = 0
+let fadeForeground = false
+let fadeForegroundAlpha = 0
+let fadeForegroundAlphaTarget = 0.5
+let curPopup = true
+let selectedNode = null
 
 function setup() {
 
     let canvasRegion = document.getElementById("canvas-region").getBoundingClientRect()
     canvas = createCanvas(windowWidth - canvasRegion.left, windowHeight - canvasRegion.top - 70);
     canvas.parent("canvas-region")
-    canvas.style("display","table-row")
     canvas.style("width","100%")
 
+    let testNodeColors = ["maroon", "violet", "cyan", "green", "orange"]
     for(let i = 0; i < 10; ++i) {
-        nodes.push(new FlowNode())
-        nodes[i].setX(i * 100)
+        nodes.push(new FlowNode(i * 100, 0, testNodeColors[i % testNodeColors.length]))
     }
 
 }
@@ -55,45 +97,99 @@ function windowResized() {
     canvas.style("width","100%")
 }
 
-
-let test_node = new FlowNode()
-let currently_dragged = null
-let drag_offx = 0, drag_offy = 0
 function draw() {
 
-    background(220);
+    clear()
 
     for (node of nodes) {
         node.draw()
+    }
+
+    if (fadeForeground) {
+
+        if(fadeForegroundAlpha < fadeForegroundAlphaTarget)
+            fadeForegroundAlpha += fadeForegroundAlphaTarget / 7
+
+        fill(`rgba(0,0,0,${fadeForegroundAlpha})`);
+        rect(-2, -2, windowWidth, windowHeight)
+
     }
 
 }
 
 function mousePressed() {
 
-    if (currently_dragged == null) {
-        for (node of nodes) {
-            if (node.isInVolume(mouseX, mouseY)) {
-                drag_offx = mouseX - node.x
-                drag_offy = mouseY - node.y
-                currently_dragged = node
+    if (fadeForeground) {
+
+    } else {
+
+        if (currently_dragged == null) {
+            for (node of nodes) {
+                if (node.isInTabVolume(mouseX, mouseY)) {
+                    drag_offx = mouseX - node.x
+                    drag_offy = mouseY - node.y
+                    currently_dragged = node
+                }
             }
+            
         }
-        
+
     }
 
 }
 
 function mouseDragged() {
 
+    dragging = true
+
     if (currently_dragged != null) {
-        currently_dragged.setX(mouseX - drag_offx)
-        currently_dragged.setY(mouseY - drag_offy)
+        currently_dragged.x = (mouseX - drag_offx)
+        currently_dragged.y = (mouseY - drag_offy)
     }
 
 }
 
 
 function mouseReleased() {
+
+    if (fadeForeground) {
+
+        fadeForegroundAlpha = 0
+        fadeForeground = false
+        curPopup.style.display="none"
+        curPopup = null
+
+    } else if (!dragging) {
+
+        for (node of nodes) {
+            if (node.isInVolume(mouseX, mouseY)) {
+                curPopup = document.getElementById("modify-node-form")
+                curPopup.style.display="block"
+                fadeForeground = true
+                selectedNode = node
+                //nodes = nodes.filter(item => item !== node)
+                break
+            }
+        }
+
+    }
+
+    dragging = false
     currently_dragged = null
+
 }
+
+
+function create() {
+    nodes.push(new FlowNode(0, 300, "orange"))
+}
+
+function onClickCreate() {
+    let btn = document.getElementById("btn-create")
+    let popup = document.getElementById("create-node-form")
+
+    popup.style.display="block"
+    curPopup = popup
+    fadeForeground = true
+}
+
