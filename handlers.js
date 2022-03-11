5c78cfd3e369abae03a4df4c41ff90913bf0c902
KosOrKosm <46112489+KosOrKosm@users.noreply.github.com>
2022-03-11 14:41:57 -0800

    Prep code for modularization - MainCanvas object

--- handlers.js
+++ handlers.js
@@ -1,26 +1,27 @@
 
 
 function onClickCreateCustomNode() {
-    let btn = document.getElementById("btn-create-custom")
-    let popup = document.getElementById("create-node-form")
+    // let btn = document.getElementById("btn-create-custom")
+    // let popup = document.getElementById("create-node-form")
 
+    window.mainCanvas.hideLastPopup()
     nodes.push(new FlowNode(0, 300, "orange"))
-    popup.style.display="none"
+
 }
 
 function onClickCreate() {
-    let btn = document.getElementById("btn-create")
-    let popup = document.getElementById("create-node-form")
+    // let btn = document.getElementById("btn-create")
+    // let popup = document.getElementById("create-node-form")
+    
+    window.mainCanvas.showPopup("create-node-form")
 
-    popup.style.display="block"
-    curPopup = popup
-    fadeForeground = true
 }
 
 function onClickDeleteSelectedNode() {
-    let btn = document.getElementById("btn-delete")
-    let popup = document.getElementById("modify-node-form")
+    // let btn = document.getElementById("btn-delete")
+    // let popup = document.getElementById("modify-node-form")
     
+    window.mainCanvas.hideLastPopup()
     nodes = nodes.filter(item => item !== selectedNode)
-    popup.style.display="none"
+    
 }
--- main-canvas.js
+++ main-canvas.js
@@ -67,116 +67,141 @@ class FlowNode {
     }
 }
 
-var canvas = null
-let nodes = []
-let dragging = false
-let currently_dragged = null
-let drag_offx = 0, drag_offy = 0
-let fadeForeground = false
-let fadeForegroundAlpha = 0
-let fadeForegroundAlphaTarget = 0.5
-let curPopup = true
-let selectedNode = null
+class MainCanvas {
 
-function setup() {
-
-    let canvasRegion = document.getElementById("canvas-region").getBoundingClientRect()
-    canvas = createCanvas(windowWidth - canvasRegion.left, windowHeight - canvasRegion.top - 70);
-    canvas.parent("canvas-region")
-    canvas.style("width","100%")
-
-    let testNodeColors = ["maroon", "violet", "cyan", "green", "orange"]
-    for(let i = 0; i < 10; ++i) {
-        nodes.push(new FlowNode(i * 100, 0, testNodeColors[i % testNodeColors.length]))
-    }
-
-}
-
-function windowResized() {
-    
-    let canvasRegion = document.getElementById("canvas-region").getBoundingClientRect()
-    resizeCanvas(windowWidth - canvasRegion.left, windowHeight - canvasRegion.top - 70);
-    canvas.style("width","100%")
-}
-
-function draw() {
-
-    clear()
-
-    for (node of nodes) {
-        node.draw()
-    }
-
-    if (fadeForeground) {
-
-        if(fadeForegroundAlpha < fadeForegroundAlphaTarget)
-            fadeForegroundAlpha += fadeForegroundAlphaTarget / 7
-
-        fill(`rgba(0,0,0,${fadeForegroundAlpha})`);
-        rect(-2, -2, windowWidth, windowHeight)
-
-    }
-
-}
-
-function mousePressed() {
-
-    if (fadeForeground) {
-
-    } else {
-
-        if (currently_dragged == null) {
-            for (node of nodes) {
-                if (node.isInTabVolume(mouseX, mouseY)) {
-                    drag_offx = mouseX - node.x
-                    drag_offy = mouseY - node.y
-                    currently_dragged = node
-                }
-            }
-            
-        }
-
-    }
-
-}
-
-function mouseDragged() {
-
-    dragging = true
-
-    if (currently_dragged != null) {
-        currently_dragged.x = (mouseX - drag_offx)
-        currently_dragged.y = (mouseY - drag_offy)
-    }
-
-}
-
-
-function mouseReleased() {
-
-    if (fadeForeground) {
-
-        fadeForegroundAlpha = 0
-        fadeForeground = false
-        curPopup.style.display="none"
-        curPopup = null
-
-    } else if (!dragging) {
-
-        for (node of nodes) {
-            if (node.isInVolume(mouseX, mouseY)) {
-                curPopup = document.getElementById("modify-node-form")
-                curPopup.style.display="block"
-                fadeForeground = true
-                selectedNode = node
-                //nodes = nodes.filter(item => item !== node)
-                break
-            }
-        }
-
-    }
-
+    canvas
+    nodes
     dragging = false
     currently_dragged = null
+    drag_offx = 0
+    drag_offy = 0
+    fadeForeground = false
+    fadeForegroundAlpha = 0
+    fadeForegroundAlphaTarget = 0.5
+    curPopup = true
+    selectedNode = null
+
+    setup() {
+
+        this.nodes = []
+
+        let canvasRegion = document.getElementById("canvas-region").getBoundingClientRect()
+        this.canvas = createCanvas(windowWidth - canvasRegion.left, windowHeight - canvasRegion.top - 70);
+        this.canvas.parent("canvas-region")
+        this.canvas.style("width","100%")
+    
+        let testNodeColors = ["maroon", "violet", "cyan", "green", "orange"]
+        for(let i = 0; i < 10; ++i) {
+            this.nodes.push(new FlowNode(i * 100, 0, testNodeColors[i % testNodeColors.length]))
+        }
+    
+    }
+    
+    windowResized() {
+        
+        let canvasRegion = document.getElementById("canvas-region").getBoundingClientRect()
+        resizeCanvas(windowWidth - canvasRegion.left, windowHeight - canvasRegion.top - 70);
+        this.canvas.style("width","100%")
+    }
+    
+    draw() {
+    
+        clear()
+    
+        for (let node of this.nodes) {
+            node.draw()
+        }
+    
+        if (this.fadeForeground) {
+    
+            if(this.fadeForegroundAlpha < this.fadeForegroundAlphaTarget)
+                this.fadeForegroundAlpha += this.fadeForegroundAlphaTarget / 7
+    
+            fill(`rgba(0,0,0,${this.fadeForegroundAlpha})`);
+            rect(-2, -2, windowWidth, windowHeight)
+    
+        }
+    
+    }
+    
+    mousePressed() {
+    
+        if (this.fadeForeground) {
+    
+        } else {
+    
+            if (this.currently_dragged == null) {
+                for (let node of this.nodes) {
+                    if (node.isInTabVolume(mouseX, mouseY)) {
+                        this.drag_offx = mouseX - node.x
+                        this.drag_offy = mouseY - node.y
+                        this.currently_dragged = node
+                    }
+                }
+                
+            }
+    
+        }
+    
+    }
+    
+    mouseDragged() {
+    
+        this.dragging = true
+    
+        if (this.currently_dragged != null) {
+            this.currently_dragged.x = (mouseX - this.drag_offx)
+            this.currently_dragged.y = (mouseY - this.drag_offy)
+        }
+    
+    }
+    
+    
+    mouseReleased() {
+    
+        if (this.fadeForeground) {
+
+            this.hideLastPopup()
+    
+        } else if (!this.dragging) {
+    
+            for (let node of this.nodes) {
+                if (node.isInVolume(mouseX, mouseY)) {
+                    this.showPopup("modify-node-form")
+                    this.selectedNode = node
+                    break
+                }
+            }
+    
+        }
+    
+        this.dragging = false
+        this.currently_dragged = null
+    
+    }
+
+    hideLastPopup() {
+        this.fadeForegroundAlpha = 0
+        this.fadeForeground = false
+        this.curPopup.style.display="none"
+        this.curPopup = null
+    }
+
+    showPopup(popupName) {
+        this.curPopup = document.getElementById(popupName)
+        this.curPopup.style.display="block"
+        this.fadeForeground = true
+    }
 
 }
+
+window.mainCanvas = new MainCanvas()
+
+// Mount main canvas functions to the locations p5 expects them
+window.setup = window.mainCanvas.setup
+window.draw = window.mainCanvas.draw
+window.windowResized = window.mainCanvas.windowResized
+window.mousePressed = window.mainCanvas.mousePressed
+window.mouseDragged = window.mainCanvas.mouseDragged
+window.mouseReleased = window.mainCanvas.mouseReleased
+
