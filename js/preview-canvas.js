eead02902d93c116b26c4029c0edde378c68e116
KosOrKosm <46112489+KosOrKosm@users.noreply.github.com>
2022-04-21 12:25:20 -0700

    Basic custom HTML scrollbar for preview canvas

--- js/preview-canvas.js
+++ js/preview-canvas.js
@@ -2,8 +2,14 @@
  * @ Author: Jacob Fano
  * @ Create Time: 2022-04-12 18:47:54
  * @ Modified by: Jacob Fano
- * @ Modified time: 2022-04-21 11:41:27
+ * @ Modified time: 2022-04-21 12:23:06
  */
+
+const scrollbar = document.getElementById('preview-scroll')
+const anchor = document.getElementById('preview-anchor')
+const container = document.getElementById('mini-canvas')
+let scrolling = false
+let scrollPivotMouseY = 0, scrollPivotAnchorY = 0
 
 class PreviewCanvas extends Canvas {
 
@@ -30,16 +36,24 @@ class PreviewCanvas extends Canvas {
         }
     }
 
+    jumpToY(y) {
+
+        this.#curOffsetY = y
+
+        // Bounds on Y offset
+        if (this.#curOffsetY > 0) {
+            this.#curOffsetY = 0
+        }
+    }
+
     draw(p5) {
-        p5.background('red')
-        p5.fill('black')
+        p5.background('white')
 
-        p5.push()
-        p5.translate(0, this.#curOffsetY)
-
-        this.nodes.forEach(node => node.draw(p5))
-        
-        p5.pop()
+        tempDrawState(p5, () => {
+            // Offset the canvas to perform the scrolling effect
+            p5.translate(0, this.#curOffsetY)
+            this.nodes.forEach(node => node.draw(p5))
+        })
     }
     
     mousePressed(p5) {
@@ -47,6 +61,10 @@ class PreviewCanvas extends Canvas {
     }
 
     mouseDragged(p5) {
+
+        if (scrolling)
+            return
+
         const container = document.getElementById("mini-canvas")
         const bounds = container.getBoundingClientRect()
         if (
@@ -63,7 +81,7 @@ class PreviewCanvas extends Canvas {
     }
 
     mouseReleased(p5) {
-
+        scrolling = false
     }
 
     mouseWheelListener(event) {
@@ -75,6 +93,67 @@ class PreviewCanvas extends Canvas {
 Object.assign(PreviewCanvas.prototype, NodesList) // Node List functionality mixin
 const previewCanvas = new PreviewCanvas()
 Canvas.injectInstance(previewCanvas, "mini-canvas", "mini-canvas")
+
+// TEST NODES
+// TODO: remove from final build of website
+let testNodeColors2 = ["#800000", "#EE82EE", "#00FFFF", "#008000", "#FFA500"]
+for(let i = 0; i < 10; ++i) {
+    let node = new FlowNode(0, i * 100)
+    node.tabColor = testNodeColors2[i % testNodeColors2.length]
+    previewCanvas.addNode(node)
+}
+
+
+// HTML Event Listeners associated with PreviewCanvas
+
+// TODO: make this height dynamic based on then number of nodes in the preview canvas
+const contentHeight = container.offsetHeight * 3
+anchor.style.height = Math.floor(container.offsetHeight / contentHeight * container.offsetHeight) + 'px'
+
+scrollbar.addEventListener('mousedown', (ev) => {
+    if(ev.target.nodeName == 'DIV') {
+        // clicked a position inside the scrollwheel, jump to that position
+        var offsetY = ev.clientY - container.offsetTop - container.offsetHeight / 2
+        var newAnchorY = offsetY - anchor.offsetHeight / 2
+
+        if (newAnchorY < 0) {
+            newAnchorY = 0
+        } else if (newAnchorY + anchor.offsetHeight > container.offsetHeight) {
+            newAnchorY = container.offsetHeight - anchor.offsetHeight
+        }
+        
+        anchor.style.top = newAnchorY + 'px'
+        previewCanvas.jumpToY(-offsetY)
+        
+    } else if(ev.target.nodeName == 'SPAN') {
+        // user is dragging the anchor, prepare to scroll
+        scrolling = true
+        scrollPivotMouseY = ev.clientY
+        scrollPivotAnchorY = anchor.offsetTop
+    }
+})
+scrollbar.addEventListener('mouseup', (ev) => {
+    scrolling = false
+    scrollPivotMouseY = ev.clientY
+    scrollPivotAnchorY = anchor.offsetTop
+})
+scrollbar.addEventListener('mousemove', (ev) => {
+    if(!scrolling)
+        return
+    
+    var offsetY = ev.clientY - scrollPivotMouseY
+    var newAnchorY = scrollPivotAnchorY + offsetY
+
+    if (newAnchorY < 0) {
+        newAnchorY = 0
+    } else if (newAnchorY + anchor.offsetHeight > container.offsetHeight) {
+        newAnchorY = container.offsetHeight - anchor.offsetHeight
+    }
+
+    anchor.style.top = newAnchorY + 'px'
+    previewCanvas.jumpToY(-offsetY)
+
+})
 
 // Update predictions as query input changes
 document.getElementById('c-query-import').addEventListener('input', (event) => {
@@ -90,12 +169,3 @@ document.getElementById('c-query-import').addEventListener('input', (event) => {
 //         occurs while the PreviewCanvas is visible
 document.getElementById('btn-add').addEventListener('click', 
     (event) => previewCanvas.windowResized(Canvas.getRegionP5("mini-canvas")))
-
-// TEST NODES
-// TODO: remove from final build of website
-let testNodeColors2 = ["#800000", "#EE82EE", "#00FFFF", "#008000", "#FFA500"]
-for(let i = 0; i < 10; ++i) {
-    let node = new FlowNode(0, i * 100)
-    node.tabColor = testNodeColors2[i % testNodeColors2.length]
-    previewCanvas.addNode(node)
-}
