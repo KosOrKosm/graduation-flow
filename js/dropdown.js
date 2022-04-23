263255d75a3515e7dbd0b4244b6220cecb5549ab
daysmachine <dscatalan@gmail.com>
2022-04-22 23:26:14 -0700

    Fix dropdown progagation
    Co-authored-by: Jacob Fano <KosOrKosm@users.noreply.github.com>

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,6 +1,21 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function onClickDropdown() {
+
+  //Stop Propagation
+  document.getElementById('myDropdown').addEventListener('mousedown', (ev) => {
+    ev.preventDefault()
+    ev.stopImmediatePropagation()
+    ev.stopPropagation()
+  })
+  
+  document.getElementById('myDropdown').addEventListener('mousemove', (ev) => {
+    ev.preventDefault()
+    ev.stopImmediatePropagation()
+    ev.stopPropagation()
+  })
+
+  //Toggle
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
