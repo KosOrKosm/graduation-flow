a662aa34ea5eebb51b8b7304b58a2191c430ee76
KosOrKosm <46112489+KosOrKosm@users.noreply.github.com>
2022-05-10 15:46:50 -0700

    Rollback server.js changes due to regression

--- js/server.js
+++ js/server.js
@@ -2,23 +2,14 @@
  * @ Author: Jacob Fano
  * @ Create Time: 2022-03-17 12:10:58
  * @ Modified by: Jacob Fano
- * @ Modified time: 2022-05-10 11:40:36
+ * @ Modified time: 2022-05-10 15:46:43
  */
 
-const e = require('express')
 const express = require('express')
-const filesys = require('fs')
 const mysql = require('mysql')
 const app = express()
 const root = __dirname + '../../'
 app.set('port', 3000)
-
-const dbConn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'student'
-})
 
 // Host assets
 app.use('/css', express.static(root + 'css/'))
@@ -37,55 +28,35 @@ app.get('/app', (req, res) => {
 app.get('/query', (req, res) => {
     
     console.log('REQ RECIEVED: /query GET')
-
-    const query = `
-        SELECT  
-            CODE_ as classPrefixNumber, 
-            TITLE as className, 
-            UNITS as classUnit, 
-            MAJOR as classMajor, 
-            DESCR as classDescription, 
-            TABCOLOR as tabColor, 
-            PREREQ as prereqs
-        from 
-            stats`
-          
-    dbConn.connect((err) => {
-        if (err) {
-
-            // Send some default nodes if the DB is unavaliable
-            console.log('cannot connect to SQL DB, falling back on dummy data')
-            filesys.readFile(__dirname + '\\dummy_nodes.json', 'utf8', (err, data) => {
-                if (err) {
-                    res.status(400).send('Failed to send data')
-                } else {
-                    res.status(200).send(data.toString())
-                }
-            })
-
-        } else {
-            
-            //made some changes to the information that is being returned
-            dbConn.query(query, (err, rows, fields) => {
-
-                if (err) throw err
-                
-                for (row of rows) {
-                        row.prereqs = row.prereqs.split(',')
-                }
-
-                //*here we are sending the result of the query.
-                res.status(200).send(JSON.stringify(rows))
-                console.log(JSON.stringify(rows))
-                //console.log(rows[0].Course + rows[0].Dept)
-                //* this can be used to test the values that are coming in from the query*\\
-            })
-            
-            dbConn.end()
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'student'
+    })
+      
+    connection.connect()
+    
+    //made some changes to the information that is being returned
+    connection.query(`SELECT  CODE_ as classPrefixNumber, TITLE as className, UNITS as classUnit, MAJOR as classMajor, DESCR as classDescription, TABCOLOR as tabColor, PREREQ as prereqs from stats `, (err, rows, fields) => {
+        // console.log(rows[20].prereqs)
+        for (row of rows) {
+             row.prereqs = row.prereqs.split(',')
         }
+
+    if (err) throw err
+    //*here we are sending the result of the query.
+    res.status(200).send(JSON.stringify(rows))
+    console.log(JSON.stringify(rows))
+    //console.log(rows[0].Course + rows[0].Dept)
+    //* this can be used to test the values that are coming in from the query*\\
     })
     
+    connection.end()
+
 })
+
+
 
 
 // =========  ABOUT PAGE  =========
