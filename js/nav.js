e7daaf76dceeda3133248cc16a93a2c37c012313
daysmachine <dscatalan@gmail.com>
2022-05-13 00:14:35 -0700

    change mobile menu to default black color

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,10 @@
 function openNav() {
    document.getElementById("myNav").style.height = "100%";
+   document.getElementById("mobile-footer").style.transitionDelay = ".3s";
+   document.getElementById("mobile-footer").style.color = "white";
 }
 
 function closeNav() {
    document.getElementById("myNav").style.height = "0%";
+   document.getElementById("mobile-footer").style.color = "black";
 }
